1b04e9eefecb2e3d00bc7dd32f5c75abb98552c8
Steven-emmanuel Lavoie <slavoie@c3r12p7.42quebec.com>
2024-03-29 12:49:05 -0400

    message

--- src/Client.cpp
+++ src/Client.cpp
@@ -50,13 +50,12 @@ bool Client::sendMessage(const std::string& message) const {
     // Ajout de "\r\n" à la fin du message pour respecter le protocole IRC
     std::string formattedMessage = message + "\r\n";
 
-    // Envoie le message sur le socket
     ssize_t bytesSent = send(socket, formattedMessage.c_str(), formattedMessage.length(), 0);
 
     if (bytesSent < 0) {
         std::cerr << "Erreur lors de l'envoi du message." << std::endl;
-        return false; // Échec de l'envoi
+        return false; 
     }
 
-    return true; // Message envoyé avec succès
+    return true; 
 }
